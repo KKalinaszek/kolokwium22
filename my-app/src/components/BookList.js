aa5f2b675c1ec048b8b2845599630bcfd5188e27
John Doe <johndoe@example.com>
2023-06-14 15:25:12 +0200

    add search bar in book list

--- my-app/src/components/BookList.js
+++ my-app/src/components/BookList.js
@@ -3,7 +3,7 @@ import { BookContext } from "../context/BookContext";
 import Book from "./Book";
 
 function BookList() {
-  const { state, search } = useContext(BookContext);
+  const { state, search, setSearch } = useContext(BookContext);
 
   const filteredBooks = state.books.filter(book =>
     book.title.toLowerCase().includes(search.toLowerCase())
@@ -11,6 +11,12 @@ function BookList() {
 
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Wyszukaj książkę"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       {filteredBooks.map(book => <Book key={book.id} book={book} />)}
     </div>
   );
